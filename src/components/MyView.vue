39ff2e825afe6cdfdbc5356df07c5e8fde947519
James Adams <jadams10152@gmail.com>
2022-06-08 17:08:08 +0100

    Added top row of main view to be used as inputs

---
+++ src/components/MyView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <v-form ref="form" v-model="valid" lazy-validation>
+            <v-container>
+                <v-row>
+                    <v-text-field v-model="name" style="margin-left: 10pt;" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+                    <v-text-field v-model="name" style="margin-left: 10pt;" :counter="10" :rules="nameRules" label="Overs Bowled (Practice)" required></v-text-field>
+                    <v-text-field v-model="name" style="margin-left: 10pt;" :counter="10" :rules="nameRules" label="Overs Bowled (Competition)" required></v-text-field>
+                    <v-btn @click="resetValidation" style="margin-top: 8pt; margin-left: 10pt;">Add Row</v-btn>
+                </v-row>
+            </v-container>
+        </v-form>
+    </div>
+</template>
